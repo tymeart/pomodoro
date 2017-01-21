1c1467daeb2de82c9c917751959f8b9bdf0d6743
Tiffany Lam <tlam5@sbcglobal.net>
2017-01-21 15:12:07 -0800

    Assign all buttons to variable names

--- js/index.js
+++ js/index.js
@@ -2,6 +2,5 @@ var intervalLength = 25;
 var breakLength = 5;
 var timer = 25;
 
-var adjustmentButtons = document.querySelectorAll('.buttons');
-
-console.log(adjustmentButtons);
+var adjustmentButtons = document.querySelectorAll('.buttons button');
+var startStopButton = document.getElementById('start-stop');
