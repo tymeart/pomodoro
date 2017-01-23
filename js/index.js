72f2d86c5bf73e7f2bb285a4718924348fd37552
Tiffany Lam <tlam5@sbcglobal.net>
2017-01-23 01:10:59 -0800

    Change session lengths displayed when + and - buttons are clicked

--- js/index.js
+++ js/index.js
@@ -4,3 +4,27 @@ var timer = 25;
 
 var adjustmentButtons = document.querySelectorAll('.buttons button');
 var startStopButton = document.getElementById('start-stop');
+var workLengthMinutes = document.querySelector('.session-length .minutes');
+var breakLengthMinutes = document.querySelector('.break-length .minutes');
+
+adjustmentButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    var btnVal = this.innerText;
+    var intervalCategory = this.className;
+
+    // minutes have to > 0 though...
+    if (intervalCategory === 'work' && btnVal === '+') {
+      workLength++;
+      workLengthMinutes.innerText = workLength;
+    } else if (intervalCategory === 'work' && btnVal === '-') {
+      workLength--;
+      workLengthMinutes.innerText = workLength;
+    } else if (intervalCategory === 'break' && btnVal === '+') {
+      breakLength++;
+      breakLengthMinutes.innerText = breakLength;
+    } else if (intervalCategory === 'break' && btnVal === '-') {
+      breakLength--;
+      breakLengthMinutes.innerText = breakLength;
+    }
+  });
+});
